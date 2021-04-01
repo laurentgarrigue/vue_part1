97c3e1c0d0e664ea3c7f68cb9df4cdf77312a1c9
laurentgarrigue <lgarrigue@gmail.com>
2021-04-01 15:01:44 +0200

    End chapter 16 : v-fo, v-on

--- assets/js/components/sidebar.vue
+++ assets/js/components/sidebar.vue
@@ -1,18 +1,33 @@
 <template>
     <div>
-        <div class="p-3 mb-5">
+        <div
+            :class="[$style.component, 'p-3', 'mb-5']"
+            :style="{ width: collapsed ? '70px' : 'auto' }"
+        >
             <h5 class="text-center">Categories</h5>
             <ul class="nav flex-column mb4">
                 <li class="nav-item">
                     <a class="nav-link" href="/">All Products</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Category A</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Category B</a>
+
+                <li
+                    v-for="(category, index) in categories"
+                    :key="index"
+                    class="nav-item"
+                >
+                    <a class="nav-link" :href="category.link">{{
+                        category.name
+                    }}</a>
                 </li>
             </ul>
+            <hr>
+            <div class="d-flex justify-content-end">
+                <button
+                    class="btn btn-secondary btn-sm"
+                    @click="toggleCollapsed"
+                    v-text="collapsed ? '>>' : '<< Collapse'"
+                />
+            </div>
         </div>
     </div>
 </template>
@@ -20,19 +35,39 @@
 <script>
 export default {
     name: 'Sidebar',
+    data() {
+        return {
+            collapsed: false,
+            categories: [
+                {
+                    name: 'Dot Matrix Printers',
+                    link: '#',
+                },
+                {
+                    name: 'Iomega Zip Drives',
+                    link: '#',
+                },
+            ],
+        };
+    },
+    methods: {
+        toggleCollapsed() {
+            this.collapsed = !this.collapsed;
+        },
+    },
 };
 </script>
 
-<style lang="scss">
-@import "~styles/components/light-component.scss";
+<style lang="scss" module>
+@import '~styles/components/light-component.scss';
 
-.sidebar {
+.component {
     @include light-component;
-}
 
-ul {
-    li a:hover {
-        background: $blue-component-link-hover;
+    ul {
+        li a:hover {
+            background: $blue-component-link-hover;
+        }
     }
 }
 </style>
