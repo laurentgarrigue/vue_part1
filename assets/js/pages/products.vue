13c423c295e927210929c0844cbe66b51ff207a1
laurentgarrigue <lgarrigue@gmail.com>
2021-04-01 10:48:22 +0200

    End chapter 13

--- assets/js/pages/products.vue
+++ assets/js/pages/products.vue
@@ -1,67 +1,26 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <aside class="sidebar col-xs-12 col-3">
-        <div class="p-3 mb-5">
-          <h5 class="text-center">Categories</h5>
-          <ul class="nav flex-column mb4">
-            <li class="nav-item">
-              <a class="nav-link" href="/">All Products</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Category A</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Category B</a>
-            </li>
-          </ul>
-        </div>
-      </aside>
-      <div class="col-xs-12 col-9">
+    <div class="container-fluid">
         <div class="row">
-          <div class="col-12">
-            <h1>Products</h1>
-          </div>
+            <aside class="sidebar col-xs-12 col-3">
+                <sidebar />
+            </aside>
+            <div class="col-xs-12 col-9">
+                <catalog />
+            </div>
         </div>
-        <div class="row">
-          <div class="col-xs-12 col-6 mb-2 pb-2">
-            TODO - load some products!
-          </div>
-        </div>
-        <div class="row">
-          <legend-component :title="legend" />
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 // import { defineComponent } from '@vue/composition-api'
-import legendComponent from "../components/legend";
+import Catalog from '../components/catalog';
+import Sidebar from '../components/sidebar';
 
 export default {
-  name: "Products",
-  components: {
-    legendComponent,
-  },
-  data() {
-    return {
-      legend: "Shipping takes 10-12 weeks, and products probably won't work",
-    };
-  },
+    name: 'Products',
+    components: {
+        Catalog,
+        Sidebar,
+    },
 };
 </script>
-
-<style lang="scss">
-@import "../../scss/components/light-component.scss";
-.sidebar {
-  @include light-component;
-}
-
-ul {
-  li a:hover {
-    background: $blue-component-link-hover;
-  }
-}
-</style>
