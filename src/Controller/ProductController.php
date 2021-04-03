a222347ba9e48344024d2d8709e1098f24a64072
laurentgarrigue <lgarrigue@gmail.com>
2021-04-03 23:35:27 +0200

    End chapter 36 - Sending json via twig

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,24 +9,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use ApiPlatform\Core\Api\IriConverterInterface;
+use App\Repository\CategoryRepository;
 
 class ProductController extends AbstractController
 {
     /**
      * @Route("/", name="app_homepage")
      */
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
-        return $this->render('product/index.html.twig');
+        return $this->render('product/index.html.twig', [
+            'categories' => $categoryRepository->findAll()
+        ]);
     }
 
     /**
      * @Route("/category/{id}", name="app_category")
      */
-    public function showCategory(Category $category, IriConverterInterface $iriConverter): Response
+    public function showCategory(Category $category, IriConverterInterface $iriConverter, CategoryRepository $categoryRepository): Response
     {
         return $this->render('product/index.html.twig', [
             'currentCategoryId' => $iriConverter->getIriFromItem($category),
+            'categories' => $categoryRepository->findAll()
         ]);
     }
     /**
