e57537bdbb8dfeea5433d1f47baad80da7e181d9
laurentgarrigue <lgarrigue@gmail.com>
2021-04-05 15:24:44 +0200

    Part 2, end chapter 3 - get current product id

--- assets/js/services/page-context.js
+++ assets/js/services/page-context.js
@@ -1,3 +1,17 @@
+/**
+ * Return the current category id that's set by the server
+ *
+ * @returns {string|null}
+ */
 export function getCurrentCategoryId() {
     return window.currentCategoryId;
+}
+
+/**
+ * Return the current product id that's set by the server
+ *
+ * @returns {string|null}
+ */
+export function getCurrentProductId() {
+    return window.currentProductId;
 }
