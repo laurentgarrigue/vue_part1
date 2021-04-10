746554a123ddf65465ad3eadc4809d19dbb66468
laurentgarrigue <lgarrigue@gmail.com>
2021-04-10 17:29:11 +0200

    End chapter 36 - checkout form, first entry

--- assets/js/components/checkout/index.vue
+++ assets/js/components/checkout/index.vue
@@ -1,7 +1,31 @@
 <template>
     <div class="row p-3">
         <div class="col-12">
-            Checkout Form
+            <form>
+                <div class="form-group">
+                    <label
+                        for="customerName"
+                        class="col-form-label"
+                    >
+                        Name
+                    </label>
+                    <input
+                        id="customerName"
+                        v-model.trim="form.customerName"
+                        type="text"
+                        :class="{
+                            'is-invalid': !isFieldValid('customerName'),
+                            'form-control': true,
+                        }"
+                    >
+                    <span
+                        v-show="!isFieldValid('customerName')"
+                        class="invalid-feedback"
+                    >
+                        {{ validationErrors.customerName }}
+                    </span>
+                </div>
+            </form>
         </div>
     </div>
 </template>
@@ -9,5 +33,23 @@
 <script>
 export default {
     name: 'CheckoutForm',
+    data() {
+        return {
+            form: {
+                customerName: '',
+                customerEmail: '',
+                customerAddress: '',
+                customerZip: '',
+                customerCity: '',
+                customerPhone: '',
+            },
+            validationErrors: {},
+        };
+    },
+    methods: {
+        isFieldValid(fieldName) {
+            return !(fieldName in this.validationErrors);
+        },
+    },
 };
 </script>
