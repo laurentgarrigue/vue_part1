ff2d1344742c279cc05661e6237eaf21e4bf350c
laurentgarrigue <lgarrigue@gmail.com>
2021-04-11 00:05:01 +0200

    End chapter 44 - client-side form validation

--- assets/js/components/checkout/index.vue
+++ assets/js/components/checkout/index.vue
@@ -15,34 +15,40 @@
                         v-model="form.customerName"
                         class="col"
                         v-bind="getFieldProps('customerName', 'Name:')"
+                        @blur="validateField"
                     />
                     <form-input
                         v-model="form.customerEmail"
                         class="col"
                         type="email"
                         v-bind="getFieldProps('customerEmail', 'Email:')"
+                        @blur="validateField"
                     />
                 </div>
                 <form-input
                     v-model="form.customerAddress"
                     v-bind="getFieldProps('customerAddress', 'Address:')"
+                    @blur="validateField"
                 />
                 <div class="form-row">
                     <form-input
                         v-model="form.customerZip"
                         class="col"
                         v-bind="getFieldProps('customerZip', 'Zip Code:')"
+                        @blur="validateField"
                     />
                     <form-input
                         v-model="form.customerCity"
                         class="col"
                         v-bind="getFieldProps('customerCity', 'City:')"
+                        @blur="validateField"
                     />
                     <form-input
                         v-model="form.customerPhone"
                         class="col"
                         type="tel"
                         v-bind="getFieldProps('customerPhone', 'Phone Number:')"
+                        @blur="validateField"
                     />
                 </div>
                 <div class="form-row justify-content-end align-items-center">
@@ -67,6 +73,7 @@ import FormInput from '@/components/checkout/form-input';
 import Loading from '@/components/loading';
 import { createOrder } from '@/services/checkout-service';
 import { clearCart } from '@/services/cart-service';
+
 export default {
     name: 'CheckoutForm',
     components: {
@@ -89,7 +96,7 @@ export default {
                 customerCity: '',
                 customerPhone: '',
             },
-            validationErrors: {},
+            validationErrors: this.getEmptyValidationErrors(),
             loading: false,
             serverError: false,
         };
@@ -112,7 +119,7 @@ export default {
         async onSubmit() {
             this.loading = true;
             this.serverError = false;
-            this.validationErrors = {};
+            this.validationErrors = this.getEmptyValidationErrors();
             try {
                 const response = await createOrder({
                     ...this.form,
@@ -132,6 +139,33 @@ export default {
             } finally {
                 this.loading = false;
             }
+        },
+        validateField(event) {
+            const validationMessages = {
+                customerName: 'Please, enter your full name!',
+                customerEmail: 'Please, enter your email address!',
+                customerAddress: 'Please, enter your street address!',
+                customerZip: 'Please, enter your zip code!',
+                customerCity: 'Please, enter your city!',
+                customerPhone: 'Please, enter a phone number',
+            };
+
+            const validationField = event.target.id;
+            if (!this.form[validationField]) {
+                this.validationErrors[validationField] = validationMessages[validationField];
+            } else {
+                this.validationErrors[validationField] = null;
+            }
+        },
+        getEmptyValidationErrors() {
+            return {
+                customerName: null,
+                customerEmail: null,
+                customerAddress: null,
+                customerZip: null,
+                customerCity: null,
+                customerPhone: null,
+            };
         },
     },
 };
